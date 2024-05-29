5da7cda05e71d099b9b96b9044dcecee7dd30c4b
Mariano Cho <81662987+marianocho@users.noreply.github.com>
2024-05-28 23:25:16 -0300

    Add files via upload

--- lab8.s
+++ lab8.s
@@ -12,27 +12,34 @@ main:
 
     jal open
     jal read
+    mv s11, a1
 
-    addi a1, a1, 3 # primeira linha
+    addi s11, s11, 3 # primeira linha
     
     li t0, 32 # t0 = ' '
     li t1, 48 # char - t1 -> int
     li t6, 10 # t6 = '\n'
 
     li s6, 0
+    li s5, 0
+    li s4, 0
     jal dimensions # ler dimensao m
     mv s0, s6 # s0 = m
+    mv a0, s0 # a0 = s0 = m
 
     li s6, 0
+    li s5, 0
+    li s4, 0
     jal dimensions # ler dimensao n
     mv s1, s6 # s1 = n
+    mv a1, s1 # a1 = s1 = n
 
     jal setCanvasSize
 
-    mul s2, s0, s1 # s2 = m x n
-    addi a1, a1, 4 # pula o valor de alfa que é sempre 255 e o '\n'
+    mul s2, a0, a1 # s2 = m x n
+    jal skipa # skippa o valor de alfa (que sempre usaremos 255)
     li t5, 255 # alfa = t5
-    li t4, 1 # contador
+    li t4, 0 # contador
 
     jal loop_pixels # ler os pixels
     
@@ -40,46 +47,110 @@ main:
     addi sp, sp, 4
     ret
 
+skipa:
+    lbu t2, 0(s11) # t2 = armazena o digito alfa[0]
+    addi s11, s11, 1 # avanca um caractere
+
+    lbu t2, 0(s11) # t2 = armazena o digito alfa[1]
+    beq t0, t2, end_s # se t2 = ' ' -> end_s
+    beq t6, t2, end_s # se t2 = '\n' -> end_s
+    addi s11, s11, 1 # avanca um caractere
+
+    lbu t2, 0(s11) # t2 = armazena o digito alfa[2]
+    beq t0, t2, end_s # se t2 = ' ' -> end_s
+    beq t6, t2, end_s # se t2 = '\n' -> end_s
+    addi s11, s11, 2 # avanca dois caracteres
+    ret
+
+    end_s:
+        addi s11, s11, 1 # avanca um caractere
+        ret
+
 loop_pixels:
-    beq s2, t4, end_pix # m x n = contador
-    lbu s3, 0(a1) # s3 = pixel
-    sb t5, 0(a2) # a2[7...0] = alfa
-    sb s3, 1(a2) # a2[15...8] = pixel
-    sb s3, 2(a2) # a2[23...16] = pixel
-    sb s3, 3(a2) # a2[31...24] = pixel
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    loop_p:
+        beq s2, t4, end_pix # m x n = contador
+        lbu s3, 0(s11) # s3 = pixel
+
+        slli a2, s3, 24 # a2[31...24] = pixel
+        slli s10, s3, 16 # a2[23...16] = pixel
+        add a2, a2, s10
+        slli s10, s3, 8 # a2[15...8] = pixel
+        add a2, a2, s10
+        add a2, a2, t5 # a2[7...0] = alfa
+        
+        jal setPixel
+        
+        addi s11, s11, 1 # avanca um caractere
+        addi t4, t4, 1 # contador++
+        
+        j loop_p
+
+    end_pix:
+        lw ra, 0(sp)
+        addi sp, sp, 4
+        ret
+
+setPixel:
+    rem a0, t4, s1 # x
+    div a1, t4, s1 # y
     li a7, 2200
     ecall
-    addi a1, a1, 1 # avanca um caractere
-    addi t4, t4, 1 # contador++
-    j loop_pixels
-
-end_pix:
     ret
 
 setCanvasSize:
-    mv a0, s0 # largura
-    mv a1, s1 # altura
     li a7, 2201
     ecall
     ret
 
 dimensions:
-    lbu t2, 0(a1) # t2 = armazena um digito lido
+    lbu t2, 0(s11) # t2 = armazena numero[0]
 
-    beq t0, t2, end_d # se t2 = ' ' -> end_d
-    beq t6, t2, end_d # se t2 = '\n' -> end_d
     sub t2, t2, t1 # char - t1 = int = t2
+    add s6, s6, t2 # s6 = t2 first digit
+    addi s11, s11, 1 # avanca um caractere
 
-    add s6, s6, t2 # s6 += t2
-    addi a1, a1, 1 # avanca um caractere
-    j dimensions
+    lbu t2, 0(s11) # t2 = armazena numero[1]
+    beq t0, t2, end_single # se t2 = ' ' -> end_d
+    beq t6, t2, end_single # se t2 = '\n' -> end_d
 
-    end_d:
-        addi a1, a1, 1 # avanca um caractere
+    sub t2, t2, t1 # char - t1 = int = t2
+    mv s5, t2 # t5 = segundo digito
+    addi s11, s11, 1 # avanca um caractere
+    
+    lbu t2, 0(s11) # t2 = armazena numero[2]
+    beq t0, t2, end_double # se t2 = ' ' -> end_d
+    beq t6, t2, end_double # se t2 = '\n' -> end_d
+
+    sub t2, t2, t1 # char - t1 = int = t2
+    mv s4, t2 # terceiro digito
+    li t3, 100
+    mul s6, s6, t3 # centena
+    li t3, 10
+    mul s5, s5, t3 # dezena
+    add s6, s6, s5 # centena + dezena
+    add s6, s6, s4 # centena + dezena + unidade
+
+    addi s11, s11, 2 # avanca dois caracteres
+
+    ret
+
+    end_single:
+        addi s11, s11, 1 # avanca um caractere
         ret # retornar para main
+
+    end_double:
+        li t3, 10
+        mul s6, s6, t3 # dezena
+        add s6, s6, s5 # unidade
+        addi s11, s11, 1 # avanca um caractere
+        ret
+
 
 read:
     la a1, input_img   # buffer
+    li a2, 262159
     li a7, 63          # syscall read (63)
     ecall
     ret
